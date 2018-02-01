352e81b750e1c0659ece71c60aebcc344a0f8e01
Nicolas Roulleau <nicolas.roulleau@gmail.com>
2018-02-01 23:35:01 +0100

    news comment

--- news/app/models/comment.rb
+++ news/app/models/comment.rb
@@ -4,8 +4,10 @@ class Comment < ApplicationRecord
 	validates :user_id, presence: true
 	validates :link_id || :comment_id, presence: true
 
-	belongs_to :user
-	belongs_to :link
+	belongs_to :user, dependent: :destroy, counter_cache: true
+	belongs_to :link, dependent: :destroy, counter_cache: true
+	belongs_to :parent, :class_name => "Comment", :foreign_key => "parent_comment_id"
+	has_many :child_comments, :class_name => "Comment", foreign_key => "child_comment_id"
 
 end
 
--- news/app/models/link.rb
+++ news/app/models/link.rb
@@ -1,13 +1,13 @@
 class Link < ApplicationRecord
 
-VALID_URL_REGEX = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
 	validates :url, presence: true, url: true, uniqueness: { case_sensitive: false }
 	validates :user_id, presence: true
 
-	belongs_to :user
+	belongs_to :user, dependent: :destroy, counter_cache: true
 	has_many :comments
 
 end
 
   # standard validation avec la gem "validate_url"
   # validates :url, url: true au lieu de format: {with: VALID_EMAIL_REGEX}
+  # VALID_URL_REGEX = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
